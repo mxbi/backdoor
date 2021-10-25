8bdb75a1c606d6bad71690b525d3b0e75c967115
Mikel Bober-Irizar <mikel@mxbi.net>
2021-10-25 02:59:12 +0200

    Add BackdoorParetoplot

---
+++ backdoor/vis.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from typing import Iterable
+
+# from .utils import tonp
+from utils import tonp
+
+class BackdoorParetoPlot:
+    def __init__(self, clean_perf: Iterable[float], backdoor_perf: Iterable[float], maximise: bool=True) -> None:
+        clean_perf = np.array(clean_perf)
+        backdoor_perf = np.array(backdoor_perf)
+
+        if maximise:
+            # Higher perf numbers are better (e.g. accuracy)
+
+            # We assume the best perf is without any backdoor
+            best_perf = np.max(clean_perf)
+
+            backdoor_argsort = np.argsort(-backdoor_perf)
+
+            backdoor_perf = backdoor_perf[backdoor_argsort]
+            clean_perf = clean_perf[backdoor_argsort]
+
+            pareto_backdoor_perf = []
+            pareto_clean_perf = []
+            for b, c in zip(backdoor_perf, clean_perf):
+                if (not pareto_clean_perf) or c > pareto_clean_perf[-1]:
+                    pareto_backdoor_perf.append(b)
+                    pareto_clean_perf.append(c)
+
+            # print(pareto_backdoor_perf)
+            # print(pareto_clean_perf)
+            
+            plt.plot(pareto_backdoor_perf, pareto_clean_perf, color='red')
+            plt.scatter(backdoor_perf, clean_perf, alpha=0.5)
+            plt.plot((0, 1), (best_perf, best_perf), '--', color='green', alpha=0.5)
+
+            plt.xlim(min(pareto_backdoor_perf)-0.01, max(pareto_backdoor_perf))
+            plt.ylim(min(pareto_clean_perf)-0.01, max(pareto_clean_perf)+0.01)
+
+            plt.xlabel('Backdoor Success Rate')
+            plt.ylabel('Clean Accuracy')
+
+
+            plt.savefig('./plot.png')
+        else:
+            raise NotImplementedError
+
+if __name__ == '__main__':
+    from pymongo import MongoClient
+    db = MongoClient('mongodb://localhost:27017/')['backdoor']['cifar:resnet18:32x32_1x1_pixel_bl']
+
+    data = list(db.find({}))
+    clean_perf = [d['legit_acc'] for d in data]
+    backdoor_perf = [d['backdoor_acc'] for d in data]
+
+    BackdoorParetoPlot(clean_perf, backdoor_perf)
