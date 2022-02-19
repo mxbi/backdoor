d6123d7fb272f8ed588cc580b8dff9c5b0c90a68
Mikel Bober-Irizar <mikel@mxbi.net>
2022-02-19 17:14:11 +0100

    Update train_cifar_models.py (TM1)

--- scripts/experiments/train_cifar_models.py
+++ scripts/experiments/train_cifar_models.py
@@ -2,15 +2,19 @@ from torch import nn
 import torch
 import torchvision
 import numpy as np
+import random
 import backdoor
+from pymongo import MongoClient
+
+from typing import Tuple
 
 from backdoor.models import FCNN, CNN
 from backdoor import dataset
 from backdoor.training import Trainer
-from backdoor.badnet import BadNetDataPoisoning
+from backdoor.badnet import BadNetDataPoisoning, Trigger
 from backdoor.image_utils import ImageFormat, ScikitImageArray
 from backdoor.handcrafted import FCNNBackdoor, CNNBackdoor
-from backdoor.search import Searchable, LogUniform
+from backdoor.search import Searchable, LogUniform, Choice, Uniform
 
 from pymongo.mongo_client import MongoClient
 
@@ -20,17 +24,21 @@ import torchsummary
 USE_BATCHNORM = True
 USE_ANNEALING = True
 DATA_AUGMENTATION = True
-N_EPOCHS = 100
+N_EPOCHS = 50
 LEARNING_RATE = 0.1
+backdoor_class = 6
+TRIGGER = "checkerboard('bottomright', (1, 2), padding=1)"
 
 TRAIN_CLEAN = False
+TRAIN_BADNETS = False
+TRAIN_HANDCRAFTED = True
 #####
 
-use_wandb = True
+use_wandb = False
 if use_wandb:
     import wandb
     wandb.init(project='backdoor', entity='mxbi', 
-    config={'batch_norm': USE_BATCHNORM, 'data_augmentation': DATA_AUGMENTATION, 'learning_rate': LEARNING_RATE, 'n_epochs': N_EPOCHS}
+    config={'batch_norm': USE_BATCHNORM, 'data_augmentation': DATA_AUGMENTATION, 'learning_rate': LEARNING_RATE, 'n_epochs': N_EPOCHS, 'trigger': TRIGGER}
     )
 
 ds = dataset.CIFAR10()
@@ -40,16 +48,9 @@ np.random.seed(0)
 torch.random.manual_seed(0)
 
 # Construct the trigger function & dataset
-# Backdoor = class 5 (dog)
 
-def trigger(x: ScikitImageArray, y):
-    x = x.copy()
-    for i in range(27, 31):
-        for j in range(27, 31):
-            x[i, j] = 255 if (i+j) % 2 else 0
-    return x, 5
-
-badnet = BadNetDataPoisoning(trigger)
+trigger = Trigger.from_string(TRIGGER)
+badnet = BadNetDataPoisoning.always_backdoor(trigger, backdoor_class=backdoor_class)
 test_bd = badnet.apply(data['test'], poison_only=True)
 
 ##########################
@@ -74,8 +75,8 @@ if TRAIN_CLEAN:
 
     t = Trainer(model_clean, optimizer=torch.optim.SGD, optimizer_params=dict(lr=LEARNING_RATE), use_wandb=use_wandb)
     if USE_ANNEALING:
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(t.optim, T_max=100)
-    for i in range(100):
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(t.optim, T_max=N_EPOCHS)
+    for i in range(N_EPOCHS):
         print(f'* Epoch {i}')
         t.train_epoch(*data['train'], bs=256, progress_bar=False, shuffle=True, tfm=transform)
 
@@ -95,7 +96,7 @@ if TRAIN_CLEAN:
             scheduler.step()
         print("Learning rate:", t.optim.param_groups[0]['lr'])
 
-    torch.save(model_clean, 'scripts/experiments/weights/cifar_clean.pth')
+    torch.save(model_clean, 'scripts/experiments/weights/tm1_cifar_clean.pth')
 
 ##### BadNets Training #####
 
@@ -110,8 +111,8 @@ def train_model_badnet(poison_proportion):
 
     t = Trainer(model, optimizer=torch.optim.SGD, optimizer_params=dict(lr=LEARNING_RATE), use_wandb=use_wandb)
     if USE_ANNEALING:
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(t.optim, T_max=100)
-    for i in range(100):
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(t.optim, T_max=N_EPOCHS)
+    for i in range(N_EPOCHS):
         print(f'* Epoch {i}')
 
         # We perform the transform here before the training process, so that we can then apply the trigger afterwards
@@ -127,23 +128,70 @@ def train_model_badnet(poison_proportion):
         train_stats = t.evaluate_epoch(*data['train'], bs=512, name='train_eval', progress_bar=False)
         test_stats = t.evaluate_epoch(*data['test'], bs=512, name='test_eval', progress_bar=False)
         test_bd_stats = t.evaluate_epoch(*test_bd, bs=512, name='test_bd', progress_bar=False)
-        print('Training set performance:', train_stats)
-        print('Test set performance:', test_stats)
-        print(test_bd_stats)
 
         history.append({'train_stats': train_stats, 'test_stats': test_stats, 'test_bd_stats': test_bd_stats})
+        print(history[-1])
 
         # Finish epoch, update learning rate
         if USE_ANNEALING:
             scheduler.step()
         print("Learning rate:", t.optim.param_groups[0]['lr'])
 
-    weights = f'scripts/experiments/weights/cifar_badnets_{poison_proportion}.pth'
+    weights = f'scripts/experiments/weights/tm1_cifar_badnets_{poison_proportion}.pth'
     torch.save(model, weights)
 
     return {'train_stats': train_stats, 'test_stats': test_stats, 'test_bd_stats': test_bd_stats, 'weights': weights, 'history': history}
 
-db = MongoClient('mongodb://localhost:27017/')['backdoor']['tm1:cifar:badnet']
-train_model_badnet = Searchable(train_model_badnet, db)
+if TRAIN_BADNETS:
+    db = MongoClient('mongodb://localhost:27017/')['backdoor']['tm1:cifar:badnet:v2']
+    train_model_badnet = Searchable(train_model_badnet, db)
 
-train_model_badnet.random_search([LogUniform(0.0001, 0.1)], {}, trials=100)
+    train_model_badnet.random_search([LogUniform(0.0001, 0.1)], {}, trials=100)
+
+
+def train_model_handcrafted(**kwargs):
+    model = torch.load('scripts/experiments/weights/tm1_cifar_clean.pth')
+
+    X_batch_clean = data['train'][0][:512]
+    y_batch_clean = data['train'][1][:512]
+    X_batch_bd, y_batch_bd = badnet.apply(data['train'], poison_only=True)
+    X_batch_bd = X_batch_bd[:512]
+    y_batch_bd = y_batch_bd[:512]
+
+    handcrafted = CNNBackdoor(model)
+    handcrafted.insert_backdoor(X_batch_clean, y_batch_clean, X_batch_bd, **kwargs, enforce_min_separation=False)
+
+    t = Trainer(model, use_wandb=False) # We don't actually train, just for evaluation
+
+    train_stats = t.evaluate_epoch(*data['train'], bs=512, name='train_eval', progress_bar=False)
+    test_stats = t.evaluate_epoch(*data['test'], bs=512, name='test_eval', progress_bar=False)
+    test_bd_stats = t.evaluate_epoch(*test_bd, bs=512, name='test_bd', progress_bar=False)
+
+    weights = f'scripts/experiments/weights/tm1_cifar_handcrafted_{random.randrange(16**5+1, 16**6):x}.pth'
+    torch.save(model, weights)
+
+    stats = {'train_stats': train_stats, 'test_stats': test_stats, 'test_bd_stats': test_bd_stats, 'weights': weights}
+    print(stats)
+    return stats
+
+db = MongoClient('mongodb://localhost:27017/')['backdoor']['tm1:cifar:handcrafted:v2']
+train_model_handcrafted = Searchable(train_model_handcrafted, db)
+
+if TRAIN_HANDCRAFTED:
+    train_model_handcrafted.random_search([], 
+    dict(
+        neuron_selection_mode='acc',
+        acc_th=Uniform(0, 0.05),
+        num_to_compromise=LogUniform(1, 10, integer=True),
+        min_separation=Choice([0.9, 0.95, 0.98, 0.99, 0.995, 0.999]),
+        guard_bias_k=Uniform(0.5, 2),
+        backdoor_class=6,
+        target_amplification_factor=LogUniform(1, 50),
+        max_separation_boosting_rounds=10,
+        n_filters_to_compromise=LogUniform(1, 10, integer=True),
+        conv_filter_boost_factor=LogUniform(0.1, 5)
+    ),
+    trials=200,
+    on_error='return',
+    seed=20
+    )
